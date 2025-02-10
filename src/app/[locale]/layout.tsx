ec207ab6514eb14ea3444254d1069a9146869760
Ricardo Dias <ricardo.dias@mandaum.com>
2025-02-10 00:44:13 -0300

    feat: enhancement to default redirect

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,12 +1,16 @@
 import { ReactElement } from "react";
 import { I18nProviderClient } from "@/locales/client";
+import QueryProvider from '@/providers/queryProvider'
+import "./globals.css";
 
-export default async function SubLayout({ params, children }: { params: Promise<{ locale: string }>, children: ReactElement }) {
+export default async function LocaleLayout({ params, children }: { params: Promise<{ locale: string }>, children: ReactElement }) {
   const { locale } = await params
  
   return (
     <I18nProviderClient locale={locale}>
-      {children}
+      <QueryProvider>
+        {children}
+      </QueryProvider>
     </I18nProviderClient>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,17 @@
-"use client";
+import { Metadata } from 'next'
 
-import { Geist, Geist_Mono } from "next/font/google";
-import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export const metadata: Metadata = {
+  title: 'My Next.js Application'
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-
-  const [queryClient] = useState(() => new QueryClient());
-
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
-    </QueryClientProvider>
-  );
+    <html>
+      <body>{children}</body>
+    </html>
+  )
 }
